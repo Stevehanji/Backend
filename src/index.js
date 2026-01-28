6ad5bdb4ba9b0878c41fd0764f90ad96bd4ed0ec
Xcode Hanji <truongcanhchan@gmail.com>
2026-01-28 14:03:12 +0700

    Add static file public

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const port = 8192;
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // HTTP logger
 // app.use(morgan("combined"));
 
@@ -16,13 +18,14 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
+    // res.send("Hello world");
     res.render("home");
 });
 
 app.get("/news",(req, res) => {
     res.render("news");
-})
+});
 
 app.listen(port, () => {
     console.log(`Example app listening at localhost:${port}`)
-})
+});
